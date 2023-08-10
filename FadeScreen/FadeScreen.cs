4806958a01107a7977af705ac68aaee84c08c53f
Mohammad-Hossein <sensimdh81@gmail.com>
2023-08-10 14:50:17 +0330

    Add files via upload

---
+++ FadeScreen/FadeScreen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class FadeScreen : MonoBehaviour
+{
+	public static FadeScreen instance;
+	public static bool FadeCompleted;
+	public float speed = 1;
+
+	public Material Screen;
+	public AudioMixer Audio;
+	
+	private float FadeTime
+	{
+		set
+		{
+			Screen.SetFloat("_FadeTime", value);
+			Audio.SetFloat("_MasterVolume", Mathf.Lerp(0, -80, value));
+		}
+	}
+	
+	private void Awake()
+	{
+		instance = this;
+		FadeIn();
+	}
+
+	public void FadeIn() => StartCoroutine(interpolate(1, 0));
+	public void FadeOut() => StartCoroutine(interpolate(0, 1));
+	
+	private IEnumerator interpolate(float from, float to)
+	{		
+		FadeCompleted = false;
+		float cur = from;
+		for (float t = 0; cur != to; t += Time.deltaTime * speed) 
+		{
+			cur = Mathf.Clamp01(Mathf.SmoothStep(from, to, t));
+			FadeTime = cur;
+			yield return null;
+		}
+		FadeCompleted = true;
+	}
+}
---
+++ FadeScreen/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+	private int NextSceneIndex
+	{
+		get
+		{
+			var i = SceneManager.GetActiveScene().buildIndex;
+			if(i >= SceneManager.sceneCountInBuildSettings)
+				i = 0;
+			return i;
+		}
+	}
+	public void FadeToNextScene()
+	{
+		FadeIntoScene(NextSceneIndex);
+	}
+	public void FadeIntoScene(int index)
+	{
+		StartCoroutine(FadeToSceneAsync(index));
+	}
+
+	private IEnumerator FadeToSceneAsync(int i)
+	{
+		yield return new WaitUntil(() => FadeScreen.FadeCompleted);
+		FadeScreen.instance.FadeOut();
+		yield return new WaitUntil(() => FadeScreen.FadeCompleted);
+		
+		var op = SceneManager.LoadSceneAsync(i);
+		while(!op.isDone)
+		{
+			yield return null;
+		}
+	}
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+            FadeToNextScene();
+    }
+
+}
